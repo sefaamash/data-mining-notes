4dc6c195bdd8ebaca5711ecffebfb0001a04c1ba
Muhammad Aamash Khan <75009862+sefaamash@users.noreply.github.com>
2022-03-24 15:59:12 +0500

    Update actualproject.py

--- Web scraping to sql database/actualproject.py
+++ Web scraping to sql database/actualproject.py
@@ -1,63 +1,62 @@
+from re import I
+from selenium import webdriver
 from bs4 import BeautifulSoup
-import requests
+import time
 import sqlite3
-import csv
-#create connection
-conn = sqlite3.connect('cputest.db')
+conn = sqlite3.connect('kse100psxjsb.db')
 c = conn.cursor()
-def data(url):
-    "Website that i am scrapping (request ;ibrary is used to get html code of the website"
-    source=requests.get(url).text
-    "Using lxml parser instead of html5lib  it has all html code"
-    soup=BeautifulSoup(source,'lxml')
-    ".pretiffy is used so that we get html code in a formatted form"
-    "print(soup.prettify())"
-    'print(soup.title.text)'
+conn.execute('''CREATE TABLE IF NOT EXISTS Shows
+              (Item TEXT, Indx INT)''')
+print("Database created and Successfully Connected to SQLite")
 
 
-    for article in soup.findAll("article"):
-        "it will go in article tag then go to h2 tag then a hen get the headline text"
-        headline=article.h2.a.text
-        print(headline)
 
-        time=article.p.time.text
-        print(time)
-
-        summary=article.div.p.text
-        print(summary)
-
-        "Making link for embeded youtube videos we write in try except beacuse the reason is if some of the video or data is missing incase then the list will stuck there it will not get to further so to avoid that exception we just set that info to none and move forward"
-        try:
-            "not finding in soup which has all html but only finding iframe inside article and in that iframe we want src"
-            vidsrc=article.find('iframe',class_='youtube-player')['src']
-
-            '.split split the link with \ means it will make a list seperated by \  so the link we want is on index 4 '
-            vidid=vidsrc.split('/')[4]
-            'now splitting that splitted link again with ? then we get the id we want that id is on 0 index afetr we split it with ?'
-            vidid=vidid.split('?')[0]
-
-
-            'now making our own link with taht extracted if we get after splitting'
-            ytlink=f"https://youtube.com/watch?v={vidid}"
-
-        except Exception as e:
-            "if info is misssing it will set it to none to keep moving the loop"
-            ytlink=None
-        print(ytlink)
-
-        'to get space after every info '
-        print()
-
-        c.execute ( '''INSERT INTO scrap VALUES(?,?,?,?)''' , (headline , time , summary , ytlink) )
-data ('https://coreyms.com' )
-conn.commit ( )
-
-print ( 'complete.' )
-
-# select all from table
-c.execute ( '''SELECT * FROM data''' )
-results = c.fetchall ( )
-print ( results )
-# close connection
-conn.close ( )
-
+while True:
+    c.execute('''DELETE FROM Shows ''')
+    conn.commit()    
+    try:
+            options=webdriver.ChromeOptions()
+            options.add_argument("--log-level=3")
+            options.add_argument("--start-maximized")
+            driver=webdriver.Chrome(chrome_options=options)
+            driver.get("https://www.psx.com.pk/")
+            page=driver.page_source
+            soup=BeautifulSoup(str(page),'html.parser')
+            rates=soup.find_all('div',{'class':'rates'})
+            Spansoup=BeautifulSoup(str(rates),'html.parser')
+            spans=soup.find_all('span',{'class':"symbolName"})
+            spans2=soup.find_all('span',{'class':"symbolRate"})
+            for span1,span2 in zip(spans,spans2):
+                
+                   spanA=span1.text
+                   print(spanA)
+                   spanB=span2.text
+                   print(spanB)
+                   #c.execute('''INSERT INTO Shows VALUES(?,?)' '',(spanA,spanB))
+                   #c.execute ( ''''UPDATE Shows SET Indx=? WHERE Item=?''',(spanB,spanA)) 
+                   #query=c.execute('''SELECT * FROM Shows WHERE Item=? EXIST ''',(spanA))
+                   #if query :
+                      #c.execute('''UPDATE SHOWS SET Indx=? WHERE ITEM =?''',(spanB,spanA))
+                   #else:
+                            #c.execute('''INSERT INTO Shows VALUES(?,?)''',(spanA,spanB))
+                  
+                   c.execute('''INSERT INTO Shows VALUES(?,?)''',(spanA,spanB))
+                   conn.commit()
+                   
+                   
+            print("Completed")
+            time.sleep(7)
+    except Exception as e:
+        spanA=None
+        spanB=None
+    print()
+    
+                   
+    c.execute ('''SELECT * FROM Shows''' )
+    results = c.fetchall ( )
+    print ( results )
+    
+   
+    # close connection
+    #conn.close ( )
+    #UPDATE Shows SET Item=? WHERE Indx=?
